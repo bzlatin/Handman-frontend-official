28856cd69ce7e17a5f67546484508548693d3057
bzlatin <btzlatin@gmail.com>
2021-09-25 00:30:05 -0400

    Fully working hangman game no styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React, { useState } from "react";
 
 function App() {
   const [usersGuess, setUsersGuess] = useState("");
+  const [shownWord, setShownWord] = useState("");
+  const [wrongGuess, setWrongGuess] = useState("");
 
-  const guess = () => {
+  const guessLetter = () => {
     fetch(`http://localhost:8080/usersGuess/${usersGuess}`)
       .then((res) => {
-        res.text().then((text) => {
-          console.log(text);
+        res.json().then((json) => {
+          console.log(json);
+          setShownWord(json.currentWord);
+          if (json.correctGuess === false) {
+            setWrongGuess(alert("You guessed an incorrect letter. Try again."));
+          }
         });
       })
       .catch((err) => {
@@ -16,11 +22,20 @@ function App() {
       });
   };
 
+  // Reset Input Field handler
+  const resetInputField = () => {
+    setUsersGuess("");
+  };
+
   return (
     <div>
+      <div>{wrongGuess}</div>
+      <div>{shownWord}</div>
       <label>Guess a letter!</label>
       <input
+        id="input-field"
         name="usersGuess"
+        maxLength="1"
         type="text"
         value={usersGuess}
         onChange={(e) => {
@@ -30,7 +45,8 @@ function App() {
       <button
         className="btn"
         onClick={() => {
-          guess();
+          guessLetter();
+          resetInputField();
         }}
       >
         <span>Enter</span>
