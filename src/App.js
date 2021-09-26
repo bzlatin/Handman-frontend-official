b9933ac968af8d38bf621c48f43070122684a6ad
bzlatin <btzlatin@gmail.com>
2021-09-25 21:40:38 -0400

    Finished week 1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
   const [usersGuess, setUsersGuess] = useState("");
   const [shownWord, setShownWord] = useState("");
   const [wrongGuess, setWrongGuess] = useState("");
+  const [userWins, setUserWins] = useState("");
 
   const guessLetter = () => {
     fetch(`http://localhost:8080/usersGuess/${usersGuess}`)
       .then((res) => {
         res.json().then((json) => {
           console.log(json);
-          setShownWord(json.currentWord);
+          setShownWord(json.displayedWord);
           if (json.correctGuess === false) {
             setWrongGuess(alert("You guessed an incorrect letter. Try again."));
+          }
+          if (json.displayedWord === json.generatedWord) {
+            setTimeout(function () {
+              setUserWins(alert("Congrats 🥳 , You win!"));
+            }, 50);
           }
         });
       })
@@ -29,13 +35,13 @@ function App() {
 
   return (
     <div>
+      <div>{userWins}</div>
       <div>{wrongGuess}</div>
       <div>{shownWord}</div>
       <label>Guess a letter!</label>
       <input
-        id="input-field"
         name="usersGuess"
-        maxLength="1"
+        minLength="1"
         type="text"
         value={usersGuess}
         onChange={(e) => {
@@ -44,6 +50,8 @@ function App() {
       />
       <button
         className="btn"
+        disabled={true && !usersGuess}
+        type="submit"
         onClick={() => {
           guessLetter();
           resetInputField();
